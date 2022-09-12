8ca11c864997c7d22d7af77a1bc09b27eb81f636
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2022-09-12 18:39:11 +0000

    Added files for encouragements and sad words. New option to turn on/off responses to sad words.

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-// Require the necessary discord.js classes
+// Require the necessary modules
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
-
 const fetch = require("node-fetch");
+const Database = require("@replit/database");
+const fs = require("fs");
 
-const sadWords = [
-  "sad", "depressed", "unhappy", "angry", "lonely", "heartbroken", "gloomy", "disappointed",
-  "hopeless", "grieved", "lost", "troubled", "miserable"];
+const db = new Database();
 
-const encouragements = [
-  "Cheer up!",
-  "Hang in there!",
-  "You are a great person!"
-]
+// Converts a file to an array
+const fileToArray = filename =>   
+  fs.readFileSync(filename).toString('UTF8').split('\n');
+
+const sadWords = fileToArray("sadWords.txt");
+const starterEncouragements = fileToArray("encouragements.txt");
+
+// Initialize the list of encouragements
+db.get("encouragements").then(encouragements => {
+  if (!encouragements || encouragements.length < 1) {
+    db.set("encouragements", starterEncouragements);
+  }
+})
+
+db.get("responding").then(value => {
+  if (value == null) {
+    db.set("responding", true);
+  }
+})
+
+// Adds a custom encouragement
+function updateEncouragements(encouragingMessage) {
+  db.get("encouragements").then(encouragements => {
+    encouragements.push(encouragingMessage);
+    db.set("encouragements", encouragements);
+  })
+}
+
+// Delete a custom encouragement
+function deleteEncouragement(index) {
+  db.get("encouragements").then(encouragements => {
+    if (encouragements.length > index) {
+      encouragements.splice(index, 1);
+      db.set("encouragements", encouragements);
+    }
+  })
+}
 
 // Fetch a random quote from Zen Quotes
 function getQuote() {
@@ -47,7 +78,7 @@ const client = new Client({
 
 // When the client is ready, run this code
 client.once("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("messageCreate", msg => {
@@ -65,15 +96,53 @@ client.on("messageCreate", msg => {
   }
   
   // Reply to the user with an encouragement if a sad word is detected
-  if (sadWords.some(word => msg.content.includes(word))) {
-    const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)];
-    msg.reply(encouragement);
-  }
+  db.get("responding").then(responding => {
+    if (responding && sadWords.some(word => msg.content.toLowerCase().includes(word))) {
+      db.get("encouragements").then(encouragements => {
+        const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)];
+        msg.reply(encouragement);
+      })
+    }
+  })
 
   // If the user is bored, ask if they want to play a game or hear a joke
   if (msg.content.toLowerCase().includes("bored")) {
     msg.reply("> Would you like to hear a joke or play a game?");
     // @TODO: implement buttons
+  }
+
+  // $new [encouragingMessage]: Adds a custom encouraging message 
+  if (msg.content.startsWith("$new")) {
+    encouragingMessage = msg.content.split("$new ")[1];
+    updateEncouragements(encouragingMessage);
+    msg.channel.send("New encouraging message added: " + "\"" + encouragingMessage + "\"");
+  }
+
+  // $del [index]: Deletes an encouraging message
+  if (msg.content.startsWith("$del")) {
+    encouragingMessage = parseInt(msg.content.split("$del ")[1]);
+    deleteEncouragement(encouragingMessage);
+    msg.channel.send("Encouraging message deleted.");
+  }
+
+  if (msg.content.startsWith("$list")) {
+    db.get("encouragements").then(encouragements => {
+      for (let i = 0; i < encouragements.length; i++) {
+        msg.channel.send(encouragements[i]);
+      }
+    })
+  }
+
+  if (msg.content.startsWith("$responding")) {
+    value = msg.content.split("$responding ")[1];
+
+    if (value.toLowerCase() == "on") {
+      db.set("responding", true);
+      msg.channel.send("Responding is on.");
+    } else {
+      db.set("responding", false);
+      msg.channel.send("Responding is off.");
+    }
   }
 });
 
