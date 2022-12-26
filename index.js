3b373cf9df5e809275cf83707270bcbf782777d3
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2022-12-25 22:18:33 -0800

    Supports registering and processing commands from multiple subdirectories in the commands directory

--- index.js
+++ index.js
@@ -16,24 +16,27 @@ const client = new Client({
     ],
 });
 
-// Add slash commands to Collection
+// Create a collection to store slash commands
 client.commands = new Collection();
 
 // Return an array of file names of Javascript files in the commands directory
 const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandFolders = fs.readdirSync(commandsPath);
 
 // Process slash commands
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ('data' in command && 'execute' in command) {
-        client.commands.set(command.data.name, command);
-    } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+commandFolders.forEach(async (dir) => {
+    const folder = fs.readdirSync(`${commandsPath}/${dir}/`).filter(file => file.endsWith('.js'));
+    for (const file of folder) {
+        const filePath = path.join(commandsPath, `${dir}/${file}`);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
     }
-}
+})
 
 // Return an array of file names of Javascript file in the events directory
 const eventsPath = path.join(__dirname, 'events');
