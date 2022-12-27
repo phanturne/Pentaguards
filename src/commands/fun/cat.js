8965a36cd6d7d68d5537d5477fa9aad325ea1e85
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2022-12-27 11:43:24 -0800

    Added /cat, /neko, /urban

---
+++ src/commands/fun/cat.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder} = require("discord.js");
+const { request } = require('undici');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('cat')
+        .setDescription('Sends a random picture of a cat'),
+    async execute(interaction, client) {
+        const message = await interaction.deferReply({
+            fetchReply: true,
+        })
+
+        const catResult = await request('https://aws.random.cat/meow');
+        const { file } = await catResult.body.json();
+        await interaction.editReply({ files: [{ attachment: file, name: 'cat.png' }] });
+    },
+};
---
+++ src/commands/fun/urban.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { request } = require('undici');
+
+// @TODO: command does not work
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('urban')
+        .setDescription('Get the urban dictionary definition of a word.'),
+    async execute(interaction, client) {
+        await interaction.deferReply();
+
+        const term = interaction.options.getString('term');
+        const query = new URLSearchParams({ term });
+
+        const dictResult = await request(`https://api.urbandictionary.com/v0/define?${query}`);
+        const { list } = await dictResult.body.json();
+
+        if (!list.length) {
+            return interaction.editReply(`No results found for **${term}**.`);
+        }
+
+        const [answer] = list;
+
+        const embed = new EmbedBuilder()
+            .setColor(0xEFFF00)
+            .setTitle(answer.word)
+            .setURL(answer.permalink)
+            .addFields(
+                { name: 'Definition', value: trim(answer.definition, 1024) },
+                { name: 'Example', value: trim(answer.example, 1024) },
+                {
+                    name: 'Rating',
+                    value: `${answer.thumbs_up} thumbs up. ${answer.thumbs_down} thumbs down.`,
+                },
+            );
+        await interaction.editReply({embeds: [embed]});
+    },
+};
+
