c905bb8fdf8d22ed13199f89c2754527cb6731d5
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2023-01-01 15:27:29 -0800

    Added ability to dynamically generate cards (apply frame onto border)

--- src/commands/tcg/claim.js
+++ src/commands/tcg/claim.js
@@ -1,7 +1,8 @@
 const Card = require(`../../schemas/cardSchema.js`);
 const Frame = require(`../../schemas/frameSchema.js`);
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require("discord.js");
 const { createCard, combineCards } = require(`../../tcgHelper/createCard.js`);
+const Canvas = require('@napi-rs/canvas');
 
 // @TODO: Allow users to claim cards and add into their collection
 module.exports = {
@@ -19,14 +20,32 @@ module.exports = {
         const newCards = []
         for (let i = 0; i < 3; i++) {
             newCards.push(
-                createCard(cards[i].id + borders[i].id,
-                    cards[i].url,
-                    borders[i].filledURL,
-                    borders[i].url,
+                await createCard(cards[i].id + borders[i].id,
+                    cards[i].id,
+                    borders[i].id,
                     borders[i].newWidth,
                     borders[i].newLength
             ));
         }
+
+
+
+        // Create a 700x250 pixel canvas and get its context
+        // The context will be used to modify the canvas
+        const canvas = Canvas.createCanvas(360, 540);
+        const context = canvas.getContext('2d');
+
+        const background = await Canvas.loadImage(newCards[0]);
+
+        // This uses the canvas dimensions to stretch the image onto the entire canvas
+        context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+        // Use the helpful Attachment class structure to process the file for you
+        const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
+
+        // interaction.editReply({ files: [attachment] });
+
+
 
         // Combine the 3 cards into 1 big image
         // const finalImage = combineCards(newCards);
@@ -36,12 +55,13 @@ module.exports = {
             .setColor(0x0099FF)
             .setTitle('Claim a Card')
             .setDescription(`You may choose 1 card. React with the respective emoji to claim it`)
-            .setImage(finalImage)
+            // .setImage(finalImage)
             .setThumbnail(interaction.user.avatarURL())
             .setAuthor({ name: interaction.user.username, iconURL: interaction.user.avatarURL()});
 
         await interaction.editReply({
             embeds: [embed],
         });
+        await interaction.followUp({files: [attachment]})
     }
 }
--- src/tcgHelper/createCard.js
+++ src/tcgHelper/createCard.js
@@ -1,14 +1,15 @@
 const sharp = require('sharp')
 
 module.exports = {
-    async createCard(comboID, cardURL, filledBorderURL, borderURL, newWidth, newLength) {
-        // Calculate the padding needed and new file name
-        const lengthPad = Math.ceil((540 - newLength) / 2);
-        const widthPad = Math.ceil((360 - newWidth) / 2);
-        const finalOutput = __dirname + comboID;
+    async createCard(comboID, cardId, borderId, newWidth, newLength) {
+        // Calculate the file names
+        const cardPath = `${__dirname}/../../assets/cards/${cardId}.png`;
+        const borderPath = `${__dirname}/../../assets/frames/${borderId}.png`;
+        const filledBorderPath = `${__dirname}/../../assets/filledFrames/${borderId}.png`;
+        const finalOutput = `${__dirname}/${comboID}.png`;
 
         // Resize card image based on the frame's new width and height
-        const resizedImg = await sharp(cardURL)
+        const resizedImg = await sharp(cardPath)
             .resize({
                 fit: sharp.fit.contain,
                 withoutEnlargement: true,
@@ -17,42 +18,49 @@ module.exports = {
             })
             .extend({
                 // Pad the image w/ invisible pixels on all sides until its the same size as before
-                top: lengthPad,
-                bottom: lengthPad,
-                left: widthPad,
-                right: widthPad,
+                top: Math.ceil((540 - newLength) / 2),
+                bottom: Math.floor((540 - newLength) / 2),
+                left: Math.ceil((360 - newWidth) / 2),
+                right: Math.floor((360 - newWidth) / 2),
                 background: { r: 0, g: 0, b: 0, alpha: 0 }
             })
-            // .toFile(`cardCombos/${comboID}-resized`);
+            // .toFile(`cardCombos/${comboID}-resized`)
             .toBuffer()
             .catch(err => {
                 console.log("Error: ", err);
             });
 
         // Apply an overlay to get the card in the shape of the filled frame
-        await sharp(filledBorderURL)
-            .toBuffer({ resolveWithObject: true })
-            .then(({ data, info }) => {
-                sharp(resizedImg.data)
-                    .composite([{
-                        input: data,
-                        blend: "dest-atop"
-                    }])
-                    .toFile(`${__dirname}/composite.png`, function(err) {
-                        console.log("Error: ", err)
-                    });
-                console.log(info);
-            })
-            .catch(err => {
-                console.log("Error: ", err);
-            });
+        let composite = await sharp(resizedImg)
+            .composite([{
+                input: filledBorderPath,
+                blend: "dest-atop"
+            }])
+            .toBuffer();
+
+        // await sharp(filledBorderPath)
+        //     .toBuffer({ resolveWithObject: true })
+        //     .then(({ data, info }) => {
+        //         composite = sharp(resizedImg)
+        //             .composite([{
+        //                 input: data,
+        //                 blend: "dest-atop"
+        //             }])
+        //             .toBuffer();
+        //             // .toFile(`${__dirname}/temp/composite.png`, function(err) {
+        //             //     console.log("Error: ", err)
+        //             // });
+        //         console.log(info);
+        //     })
+        //     .catch(err => {
+        //         console.log("Error: ", err);
+        //     });
 
         // Apply an overlay to frame-shaped card to add the actual frame on top
-        await sharp(`${__dirname}/composite.png`)
-            .resize(512, 512)
+        await sharp(composite)
             .composite([
                 {
-                    input: `${__dirname}/Golden_Luxurious_Card_Front.png`,
+                    input: borderPath,
                     top: 0,
                     left: 0,
                 },
