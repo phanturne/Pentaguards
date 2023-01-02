27bf98287c2c125d12c05da07c2984d9592030c6
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2023-01-02 14:29:39 -0800

    Changed borders. Added program to create borders

--- src/tcgHelper/createCard.js
+++ src/tcgHelper/createCard.js
@@ -2,6 +2,11 @@ const sharp = require('sharp')
 
 module.exports = {
     async createCard(comboID, cardId, borderId, newWidth, newLength, lengthShift, widthShift) {
+        // const padtop = Math.ceil((540 - newLength) / 2) - lengthShift
+        // const padbottom = Math.floor((540 - newLength) / 2) + lengthShift
+        // const padleft = Math.ceil((360 - newWidth) / 2) - widthShift
+        // const padright = Math.floor((360 - newWidth) / 2) + widthShift
+
         // Calculate the file names
         const cardPath = `${__dirname}/../../assets/cards/${cardId}.png`;
         const borderPath = `${__dirname}/../../assets/frames/${borderId}.png`;
@@ -15,10 +20,14 @@ module.exports = {
             })
             .extend({
                 // Pad the image w/ invisible pixels on all sides until its the same size as before
-                top: Math.ceil((540 - newLength) / 2) - lengthShift,
-                bottom: Math.floor((540 - newLength) / 2) + lengthShift,
-                left: Math.ceil((360 - newWidth) / 2) - widthShift,
-                right: Math.floor((360 - newWidth) / 2) + widthShift,
+                // top: Math.ceil((540 - newLength) / 2) - lengthShift,
+                // bottom: Math.floor((540 - newLength) / 2) + lengthShift,
+                // left: Math.ceil((360 - newWidth) / 2) - widthShift,
+                // right: Math.floor((360 - newWidth) / 2) + widthShift,
+                top: Math.ceil((540 - newLength) / 2),
+                bottom: Math.floor((540 - newLength) / 2),
+                left: Math.ceil((360 - newWidth) / 2),
+                right: Math.floor((360 - newWidth) / 2),
                 background: { r: 0, g: 0, b: 0, alpha: 0 }
             })
             .toBuffer()
