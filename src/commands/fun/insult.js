33dd96a6bcda99b3db96c6c33fa3d56173eb271c
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2023-01-26 10:41:27 -0800

    Added /insult command with targetting options

--- src/commands/fun/insult.js
+++ src/commands/fun/insult.js
@@ -2,13 +2,20 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('inspire')
-		.setDescription('Sends a random inspirational quote'),
+		.setName('insult')
+		.setDescription('Send a random insult (no hard feelings)')
+		.addUserOption(option => option.setName('target')
+			.setDescription('The user\'s avatar to show')
+			.setRequired(false)),
 	async execute(interaction) {
 		await interaction.deferReply();
 
+		// Get target user
+		const target = interaction.options.getUser('target');
+		const targetVal = target ? `<@${target.id}>` : '';
+
 		// Fetch an inspirational quote
-		const response = await fetch('https://api.quotable.io/random');
+		const response = await fetch('https://evilinsult.com/generate_insult.php?type=json');
 		const data = await response.json();
 
 		// Create the embed with the quote (or an error message if the fetch failed)
@@ -16,7 +23,7 @@ module.exports = {
 		if (response.ok) {
 			embed = new EmbedBuilder()
 				.setColor(0x0099FF)
-				.setDescription(`${data["content"]} — ${data["author"]}`);
+				.setDescription(`${targetVal} ${data["insult"]}`);
 		} else {
 			embed = new EmbedBuilder()
 				.setColor(0xFF0000)
